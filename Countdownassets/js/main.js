8afe42f116ab2a8ccbe87a48459f4c8602470c24
Peng Zhang <workpeng@vip.qq.com>
2025-09-01 13:29:18 +0800

    time change

--- Countdownassets/js/main.js
+++ Countdownassets/js/main.js
@@ -22,7 +22,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2025/09/26 18:00:00',function(event){
+     jQuery('#clock').countdown('2025/09/26 18:18:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
